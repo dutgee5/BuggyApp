110a76fdb051b7dba29e8dc3e2a5ac2971ff307d
theDone <done.muzaffer5@gmail.com>
2025-09-08 16:35:54 +0300

    UI update and corrected model in main thread

--- BuggyApp/Model/ToDo.swift
+++ BuggyApp/Model/ToDo.swift
@@ -5,4 +5,9 @@ struct ToDo: Codable {
     let title: String
     let completed: Bool
     let userID: Int
-}
+    
+    enum ToDoKeys: String, CodingKey {
+        case id, title, completed
+        case userID = "userId"
+    }
+}  
--- BuggyApp/Network/APIService.swift
+++ BuggyApp/Network/APIService.swift
@@ -9,10 +9,19 @@ class APIService {
 
     func fetchTodos() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/todos") else { return }
-        URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data {
-                let todos = try? JSONDecoder().decode([ToDo].self, from: data)
-                self.delegate?.didFetchTodos(todos ?? [])
+        URLSession.shared.dataTask(with: url) { data, _, err in
+            if let err = err {
+                print("Error fetching todos: \(err.localizedDescription)")
+                return
+            }
+            guard let data = data else { return }
+            do {
+                let todos = try JSONDecoder().decode([ToDo].self, from: data)
+                DispatchQueue.main.async {
+                    self.delegate?.didFetchTodos(todos)
+                }
+            } catch let err{
+                print("Decoding error: \(err.localizedDescription)")
             }
         }.resume()
     }
